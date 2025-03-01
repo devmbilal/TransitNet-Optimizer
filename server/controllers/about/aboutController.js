d3386545e31a858282f4d724d36d16c8b959fd14
devmbilal <info.bilalshakeel@gmail.com>
2025-03-02 02:52:07 +0500

    Enhance About page with title; update login and signup titles and add favicon; improve sidebar navigation with new links and icons

--- server/controllers/about/aboutController.js
+++ server/controllers/about/aboutController.js
@@ -1,7 +1,11 @@
 exports.about = async (req, res) => {
+
+    const locals = {
+        title: 'About Us',
+    }
     
     try {
-      res.render('pages/about/about');
+      res.render('pages/about/about', locals);
     } catch (error) {
       console.log(error);
     }
